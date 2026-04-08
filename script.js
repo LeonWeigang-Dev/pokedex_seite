6d33c6ceaa13bb25514afcaa0c6dea7675825765
Leon W. <orlando.game@web.de>
2026-04-08 17:24:28 +0200

    added favicon, new css classes and modified renderContent function and the template function

--- script.js
+++ script.js
@@ -1,20 +1,102 @@
+const pokemonTypes = [{
+    name: "Normal",
+    value: "normal",
+    color: "#A8A77A",
+}, {
+    name: "Fire",
+    value: "fire",
+    color: "#EE8130",
+}, {
+    name: "Water",
+    value: "water",
+    color: "#6390F0",
+}, {
+    name: "Grass",
+    value: "grass",
+    color: "#7AC74C",
+}, {
+    name: "Electric",
+    value: "electric",
+    color: "#F7D02C",
+}, {
+    name: "Ice",
+    value: "ice",
+    color: "#96D9D6",
+}, {
+    name: "Fighting",
+    value: "fighting",
+    color: "#C22E28",
+}, {
+    name: "Poison",
+    value: "poison",
+    color: "#A33EA1",
+}, {
+    name: "Ground",
+    value: "ground",
+    color: "#E2BF65",
+}, {
+    name: "Flying",
+    value: "flying",
+    color: "#A98FF3",
+}, {
+    name: "Psychic",
+    value: "psychic",
+    color: "#F95587",
+}, {
+    name: "Bug",
+    value: "bug",
+    color: "#A6B91A",
+}, {
+    name: "Rock",
+    value: "rock",
+    color: "#B6A136",
+}, {
+    name: "Ghost",
+    value: "ghost",
+    color: "#735797",
+}, {
+    name: "Dragon",
+    value: "dragon",
+    color: "#6F35FC",
+}, {
+    name: "Steel",
+    value: "steel",
+    color: "#B7B7CE",
+}, {
+    name: "Dark",
+    value: "dark",
+    color: "#705746",
+}, {
+    name: "Fairy",
+    value: "fairy",
+    color: "#D685AD",
+},];
 
-function init() { //Hier wird die Funktion init aufgerufen, um die Anwendung zu starten. Diese Funktion wird verwendet, um alle notwendigen Schritte auszuführen, um die Anwendung zum Laufen zu bringen, wie z.B. das Laden von Daten oder das Einrichten von Event-Listenern.
-    loadData(); //Hier wird die Funktion loadData aufgerufen, um Daten zu laden. Diese Funktion ist asynchron, da sie auf eine Antwort von einem Server wartet, um die Daten zu erhalten. Sobald die Daten geladen sind, werden sie in der Funktion renderContent angezeigt.
+
+
+
+
+
+function init() {
+    loadData();
 }
 
 
-async function loadData(path = "") { //Hier wird die Funktion aufgerufen, um Daten zu laden. Der Pfad ist optional, da er bereits in der Funktion definiert ist.
-    
+async function loadData() {
     const Base_URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
 
-    let response = await fetch(Base_URL + path + ".json"); //Hier wird die Fetch-Funktion aufgerufen, um Daten von der angegebenen URL zu erhalten. Der Pfad wird an die Basis-URL angehängt, und ".json" wird hinzugefügt, um die Daten im JSON-Format zu erhalten.
-    let responsetoJson = await response.json(); //Hier wird die Antwort der Fetch-Funktion in JSON umgewandelt, damit sie leichter zu verarbeiten ist.
-    renderContent(responsetoJson); //Hier wird die Funktion renderContent aufgerufen, um die erhaltenen Daten anzuzeigen. Die Daten werden als Argument übergeben, damit sie in der Funktion verwendet werden können.
-} 
+    let response = await fetch(Base_URL);
+    let responsetoJson = await response.json();
+    renderContent(responsetoJson);
+}
 
-/* function renderContent(responsetoJson) {
-    let contentRef = document.getElementById("mainContent"); 
-    contentRef.innerHTML = "";
-    contentRef.innerHTML = getContentTemplate(responsetoJson);
-} */
+function renderContent(responsetoJson) {
+    let contentRef = document.getElementById("mainContent");
+    let htmlContent = "";
+
+    for (let index = 0; index < responsetoJson.results.length; index++) {
+
+        htmlContent += getContentTemplate(responsetoJson, index);
+    }
+    contentRef.innerHTML = htmlContent;
+}
--- templates.js
+++ templates.js
@@ -1,6 +1,19 @@
 
-function getContentTemplate(responsetoJson) {
-    return `<div class="content">
-    <span>Hey ich bin ${responsetoJson.results[0].name}, ${responsetoJson.results[0].name} Jahre alt und wurde 1998 in ${responsetoJson.results[0].name} geboren.</span>
-</div>`;
+function getContentTemplate(responsetoJson, index) {
+    let pokemonName = responsetoJson.results[index].name;
+    let pokemonId = index + 1;
+
+    return `<section tabindex="0"  class="smallCard">
+            <div class="cardBg">
+                <span>#${pokemonId}</span>
+                <span>${pokemonName}</span>
+            </div>
+            <div>
+                <img class="smallCardImg" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png" alt="${pokemonName}">
+            </div>
+            <div class="cardBg typeIconsContainer">
+                <img class="typeIcons" src="./img/Type=Grass.svg" alt="Grass Type">
+                <img class="typeIcons" src="./img/Type=Poison.svg" alt="Poison Type">
+            </div>
+        </section>`;
 }
