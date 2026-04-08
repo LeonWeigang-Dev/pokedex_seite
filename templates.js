51cdfa73edd62d56664f4c8b5356993007ab7a33
Leon W. <orlando.game@web.de>
2026-04-08 14:36:18 +0200

    added loadingspinner img and tested some new functions

--- templates.js
+++ templates.js
@@ -1,6 +1,6 @@
 
 function getContentTemplate(responsetoJson) {
     return `<div class="content">
-    <span>Hey ich bin ${responsetoJson.results[0].name}, ${responsetoJson.results[1].name} Jahre alt und wurde 1998 in ${responsetoJson.results[1].name} geboren.</span>
+    <span>Hey ich bin ${responsetoJson.results[0].name}, ${responsetoJson.results[0].name} Jahre alt und wurde 1998 in ${responsetoJson.results[1].name} geboren.</span>
 </div>`;
 }
