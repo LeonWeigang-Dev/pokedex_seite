906ff7b13095a37e363b823f17c78e2c6242694d
Leon W. <orlando.game@web.de>
2026-04-08 19:07:55 +0200

    added fuctionality for background color and the PokemonType icons dynamically

--- script.js
+++ script.js
@@ -2,74 +2,92 @@ const pokemonTypes = [{
     name: "Normal",
     value: "normal",
     color: "#A8A77A",
+    icon: "./img/Type=Normal.svg",
 }, {
     name: "Fire",
     value: "fire",
     color: "#EE8130",
+    icon: "./img/Type=Fire.svg",
 }, {
     name: "Water",
     value: "water",
     color: "#6390F0",
+    icon: "./img/Type=Water.svg",
 }, {
     name: "Grass",
     value: "grass",
     color: "#7AC74C",
+    icon: "./img/Type=Grass.svg",
 }, {
     name: "Electric",
     value: "electric",
     color: "#F7D02C",
+    icon: "./img/Type=Electric.svg",
 }, {
     name: "Ice",
     value: "ice",
     color: "#96D9D6",
+    icon: "./img/Type=Ice.svg",
 }, {
     name: "Fighting",
     value: "fighting",
     color: "#C22E28",
+    icon: "./img/Type=Fighting.svg",
 }, {
     name: "Poison",
     value: "poison",
     color: "#A33EA1",
+    icon: "./img/Type=Poison.svg",
 }, {
     name: "Ground",
     value: "ground",
     color: "#E2BF65",
+    icon: "./img/Type=Ground.svg",
 }, {
     name: "Flying",
     value: "flying",
     color: "#A98FF3",
+    icon: "./img/Type=Flying.svg",
 }, {
     name: "Psychic",
     value: "psychic",
     color: "#F95587",
+    icon: "./img/Type=Psychic.svg",
 }, {
     name: "Bug",
     value: "bug",
     color: "#A6B91A",
+    icon: "./img/Type=Bug.svg",
 }, {
     name: "Rock",
     value: "rock",
     color: "#B6A136",
+    icon: "./img/Type=Rock.svg",
 }, {
     name: "Ghost",
     value: "ghost",
     color: "#735797",
+    icon: "./img/Type=Ghost.svg",
 }, {
     name: "Dragon",
     value: "dragon",
     color: "#6F35FC",
+    icon: "./img/Type=Dragon.svg",
 }, {
     name: "Steel",
     value: "steel",
     color: "#B7B7CE",
+    icon: "./img/Type=Steel.svg",
 }, {
     name: "Dark",
     value: "dark",
     color: "#705746",
+    icon: "./img/Type=Dark.svg",
 }, {
     name: "Fairy",
     value: "fairy",
     color: "#D685AD",
+    icon: "./img/Type=Fairy.svg"
 },];
 
 
@@ -83,20 +101,33 @@ function init() {
 
 
 async function loadData() {
-    const Base_URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
-
-    let response = await fetch(Base_URL);
+    const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+    let response = await fetch(url);
     let responsetoJson = await response.json();
-    renderContent(responsetoJson);
+    renderContent(responsetoJson.results);
 }
 
-function renderContent(responsetoJson) {
+async function renderContent(pokemonList) {
     let contentRef = document.getElementById("mainContent");
-    let htmlContent = "";
-
-    for (let index = 0; index < responsetoJson.results.length; index++) {
-
-        htmlContent += getContentTemplate(responsetoJson, index);
+    contentRef.innerHTML = "";
+    for (let i = 0; i < pokemonList.length; i++) {
+        let pokemonData = await fetchPokemonDetails(pokemonList[i].url);
+        let typeInfos = getAllTypeInfos(pokemonData);
+        contentRef.innerHTML += getContentTemplate(pokemonData, typeInfos);
     }
-    contentRef.innerHTML = htmlContent;
+}
+async function fetchPokemonDetails(url) {
+    let response = await fetch(url);
+    return await response.json();
+}
+
+function getAllTypeInfos(pokemonData) {
+    let types = pokemonData.types;
+    let infos = [];
+    for (let i = 0; i < types.length; i++) {
+        let typeName = types[i].type.name;
+        let info = pokemonTypes.find(t => t.value === typeName);
+        infos.push(info || { color: '#6d6d6d', icon: '' });
+    }
+    return infos;
 }
--- templates.js
+++ templates.js
@@ -1,19 +1,26 @@
 
-function getContentTemplate(responsetoJson, index) {
-    let pokemonName = responsetoJson.results[index].name;
-    let pokemonId = index + 1;
+function formatPokemonName(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
-    return `<section tabindex="0"  class="smallCard">
-            <div class="cardBg">
-                <span>#${pokemonId}</span>
-                <span>${pokemonName}</span>
+function getTypeIconsHtml(typeInfos) {
+    let html = "";
+    for (let i = 0; i < typeInfos.length; i++) {
+        html += `<img class="typeIcons" src="${typeInfos[i].icon}" alt="Type">`;
+    }
+    return html;
+}
+
+function getContentTemplate(pokemonData, typeInfos) {
+    let name = formatPokemonName(pokemonData.name);
+    let img = pokemonData.sprites.other['official-artwork'].front_default;
+    let iconsHtml = getTypeIconsHtml(typeInfos);
+    return `
+        <section tabindex="0" role="button" class="smallCard">
+            <div class="cardBg"><span>#${pokemonData.id}</span><span>${name}</span></div>
+            <div class="imgBg" style="background-color: ${typeInfos[0].color}">
+                <img class="smallCardImg" src="${img}" alt="${name}">
             </div>
-            <div>
-                <img class="smallCardImg" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png" alt="${pokemonName}">
-            </div>
-            <div class="cardBg typeIconsContainer">
-                <img class="typeIcons" src="./img/Type=Grass.svg" alt="Grass Type">
-                <img class="typeIcons" src="./img/Type=Poison.svg" alt="Poison Type">
-            </div>
+            <div class="cardBg typeIconsContainer">${iconsHtml}</div>
         </section>`;
 }
